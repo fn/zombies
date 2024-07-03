55b30f4061af5f88dd26c1782711cf890958de09
riley <151683389+fn@users.noreply.github.com>
2024-07-03 15:49:57 -0400

    Remove lab code. Fix it to use player components

---
+++ dev2_prototype/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zombies
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] MovementComponent Movement;
+        [SerializeField] ViewComponent View;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
