56fb2df525e3d272d4f45d791eb814890aea7dc1
porc-upin <39223318+porc-upin@users.noreply.github.com>
2024-07-07 23:45:53 -0400

    AI 0.1.1
    removed unneeded StateManager file

--- dev2_prototype/Assets/Scripts/Enemy AI/ZombieStateManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ZombieStateManager : MonoBehaviour
-{
-    //array of dropped weapons planned : updated when players drop or buy new weapons. tracks them to see which one zombies can pick up or which one is reserved
-}
