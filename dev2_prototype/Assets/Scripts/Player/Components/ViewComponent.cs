a47601b63a277bde76d2f4ba4de5f830bf3cb273
fn <151683389+fn@users.noreply.github.com>
2024-07-09 12:04:05 -0400

    Remove non-existant method

--- dev2_prototype/Assets/Scripts/Player/Components/ViewComponent.cs
+++ dev2_prototype/Assets/Scripts/Player/Components/ViewComponent.cs
@@ -80,7 +80,7 @@ namespace Zombies
         {
             // TODO: Replace all this crap with an animator.
             // TODO: Check against the weapon itself. This will add recoil regardless if we really shot.
-            if (Input.GetButtonDown("Fire1") && GameManager.Instance.LocalPlayer.HeldWeapon.IsShooting)
+            if (Input.GetButtonDown("Fire1"))
             {
                 animator.SetTrigger("Fire");
             }
