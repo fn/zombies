750e535c178a63cb13455c1bd6faed9093297e93
MitchellReames <MWReames@student.fullsail.edu>
2024-07-18 19:30:32 -0400

    (UNFINISHED) Interact script changed and able to be attached to objects.

--- dev2_prototype/Assets/Scripts/Interact/Interact.cs
+++ dev2_prototype/Assets/Scripts/Interact/Interact.cs
@@ -2,72 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Zombies;
 
 public class Interact : MonoBehaviour
 {
 
-    public GameObject ExplosiveBarrelOnPlayer;
-    public GameObject PickupText;
-    public GameObject DropText;
-    bool somethingInHand;
+    public GameObject ExplosiveBarrel;
+    public Transform ItemParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        ExplosiveBarrelOnPlayer.SetActive(false);
-        PickupText.SetActive(false);
+        ExplosiveBarrel.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            drop();
+        }
     }
 
 
-    // if player is in range of an item tagged "Pickup"
-    //display a prompt to the player that the item is able to be picked up
-    //if they press the interact button with the prompt up
-    //Attatch the item on the map to the end of the gun.
-    //display a drop item prompt with the interact Key.
+        // if player is in range of an item tagged "Pickup"
+        //display a prompt to the player that the item is able to be picked up
+        //if they press the interact button with the prompt up
+        //Attatch the item on the map to the end of the gun.
+        //display a drop item prompt with the interact Key.
 
-    //When the user drops the item
-    //The item will be removed from the end of the gun and placed a distance away from the player's forward direction.
+        //When the user drops the item
+        //The item will be removed from the end of the gun and placed a distance away from the player's forward direction.
 
+    void drop()
+    {
+        ItemParent.DetachChildren();
+        ExplosiveBarrel.transform.eulerAngles = new Vector3(ExplosiveBarrel.transform.position.x, ExplosiveBarrel.transform.position.z, ExplosiveBarrel.transform.position.y);
+        ExplosiveBarrel.GetComponent<Rigidbody>().isKinematic = false;
+        ExplosiveBarrel.GetComponent<MeshCollider>().enabled = true;
+    }
+
+    void Pickup()
+    {
+        ExplosiveBarrel.GetComponent<Rigidbody>().isKinematic = true;
+
+        ExplosiveBarrel.transform.position = ItemParent.transform.position;
+        ExplosiveBarrel.transform.rotation = ItemParent.transform.rotation;
+        ExplosiveBarrel.GetComponent<MeshCollider>().enabled = false;
+        ExplosiveBarrel.transform.SetParent(ItemParent);
+    }
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "Player" && somethingInHand == false)
+        if (other.gameObject.tag == "Player")
         {
-            PickupText.SetActive(true);
             if (Input.GetKey(KeyCode.E))
             {
-                somethingInHand = true;
-                this.gameObject.SetActive(false);
-                ExplosiveBarrelOnPlayer.SetActive(true);
-                PickupText.SetActive(false);
+                Pickup();
             }
         }
     }
-    public void OnTriggerExit(Collider other)
-    {
-        PickupText.SetActive(false);   
-    }
-
-    public void dropItem()
-    {
-        if(somethingInHand == true)
-        {
-            DropText.SetActive(true);
-            if (Input.GetKey(KeyCode.E))
-            {
-                somethingInHand= false;
-                this.gameObject.SetActive(true);
-                ExplosiveBarrelOnPlayer.SetActive(false);
-                PickupText.SetActive(false);
-            }
-        }
-    }
-
 }
