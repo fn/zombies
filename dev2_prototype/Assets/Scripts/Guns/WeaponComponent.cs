bdd4834edbd1bd62d988038508f692ef2c8d64b3
Ridge4472 <aidan.p.frawley@gmail.com>
2024-07-23 13:36:43 -0400

    Update WeaponComponent.cs
    alright, should so

--- dev2_prototype/Assets/Scripts/Guns/WeaponComponent.cs
+++ dev2_prototype/Assets/Scripts/Guns/WeaponComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [System.Serializable]
@@ -21,17 +22,22 @@ public class WeaponComponent : MonoBehaviour
     public WeaponStats Info;
 
     public string layer;
-    public GameObject Model;
-
+    public bool isShotgun; // Add this variable
     [SerializeField] GameObject Bullet_Standard;
-   
-    public int CurrentAmmo;
-    public int RemainingAmmo;
+    [SerializeField] GameObject Bullet_Shotgun;
+    [SerializeField] GameObject Mine;
+    int usedAmmo;
+    public int remainingAmmo;
+    public int shotgunPellets = 9; // Number of pellets for shotgun
+    public float spreadAngle = 5f; // Spread angle for shotgun
+    public float reloadTime = 2f; // Reload time in seconds
 
-    public bool HasAmmo { get => CurrentAmmo > 0; }
-    public bool CanReload { get => CurrentAmmo != Info.MagSize && RemainingAmmo > 0; }
+
+    public bool HasAmmo { get => currentAmmo > 0; }
+    public bool CanReload { get => currentAmmo != magSize && remainingAmmo > 0; }
 
     private float lastShotTime;
+    private bool isReloading; // Add this variable
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +51,7 @@ public class WeaponComponent : MonoBehaviour
         CurrentAmmo = Info.MagSize;
         RemainingAmmo = Info.AmmoCapacity;
         lastShotTime = 0f;
+        isReloading = false; // Initialize isReloading
     }
 
     // Update is called once per frame
@@ -55,8 +62,11 @@ public class WeaponComponent : MonoBehaviour
 
     public void Shoot(Vector3 origin, Vector3 direction)
     {
-        float shotCooldown = 1f / Info.FireRate;
-        if (CurrentAmmo <= 0 && !Info.InfiniteAmmo)
+        if (isReloading)
+            return; // Prevent shooting while reloading
+
+        float shotCooldown = 1f / rateOfFire;
+        if (currentAmmo <= 0 && !infAmmo)
             return;
 
         if (Time.time - lastShotTime >= shotCooldown)
@@ -67,16 +77,38 @@ public class WeaponComponent : MonoBehaviour
                 CurrentAmmo--;
             }
 
-            for (int i = 0; i < Info.ProjectilesPerShot; i++)
+            if (isShotgun)
             {
-                // Instantiate the bullet
-                Vector3 spreadDir = direction + (Random.insideUnitSphere * Info.SpreadFactor);
-
-                GameObject bullet = Instantiate(Bullet_Standard, origin, Quaternion.LookRotation(spreadDir));
-                if (bullet == null)
+                // Instantiate multiple bullets for shotgun
+                for (int i = 0; i < shotgunPellets; i++)
                 {
-                    return;
+                    Vector3 spreadDirection = Quaternion.Euler(
+                        Random.Range(-spreadAngle, spreadAngle),
+                        Random.Range(-spreadAngle, spreadAngle),
+                        0) * direction;
+
+                    SpawnBullet(origin, spreadDirection);
                 }
+            }
+            else
+            {
+                // Instantiate a single bullet for regular gun
+                SpawnBullet(origin, direction);
+            }
+
+            lastShotTime = Time.time;
+        }
+    }
+
+    private void SpawnBullet(Vector3 origin, Vector3 direction)
+    {
+        if (!isShotgun)
+        {
+            GameObject bullet = Instantiate(Bullet_Standard, origin, Quaternion.LookRotation(direction));
+            if (bullet == null)
+            {
+                return;
+            }
 
                 bullet.layer = LayerMask.NameToLayer(layer);
 
@@ -91,19 +123,54 @@ public class WeaponComponent : MonoBehaviour
                     rb.velocity = spreadDir * bulletDamage.speed;
                 }
 
-                // Destroy the bullet after a certain time
-                Destroy(bullet, bulletDamage.destroyTime);
+            // Destroy the bullet after a certain time
+            Destroy(bullet, bulletDamage.destroyTime);
+        }
+        else
+        {
+            GameObject bullet = Instantiate(Bullet_Shotgun, origin, Quaternion.LookRotation(direction));
+            if (bullet == null)
+            {
+                return;
             }
 
-            lastShotTime = Time.time;
+            bullet.layer = LayerMask.NameToLayer(layer);
+
+            // Transfer the damage value to the bullet
+            DamageSource bulletDamage = bullet.GetComponent<DamageSource>();
+            bulletDamage.SetDamage(damage);
+
+            // Apply velocity to the bullet
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = direction * bulletDamage.speed;
+            }x
+
+            // Destroy the bullet after a certain time
+            Destroy(bullet, bulletDamage.destroyTime);
         }
     }
 
     public void Reload()
     {
+        if (isReloading)
+            return; // Prevent multiple reload calls
+
+        StartCoroutine(ReloadCoroutine());
+    }
+    
+    private IEnumerator ReloadCoroutine()
+    {
+        isReloading = true;
+
+        // Optionally, you could add code here to show reloading animation or UI
+        yield return new WaitForSeconds(reloadTime);
+
+        // Reload logic here
         // No more ammos :C
-        if (RemainingAmmo < 0)
-            return;
+        if (remainingAmmo < 0)
+            yield break;
 
         // How much ammo we need to reload.
         int neededAmmo = Info.MagSize - CurrentAmmo;
@@ -113,6 +180,7 @@ public class WeaponComponent : MonoBehaviour
         CurrentAmmo += neededAmmo > RemainingAmmo ? RemainingAmmo : neededAmmo;
 
         // Remove it from our stockpile.
-        RemainingAmmo -= (CurrentAmmo - oldAmmo);
+        remainingAmmo -= (currentAmmo - oldAmmo);
+        isReloading = false;
     }
 }
